ab81eb846a5390050243d1e74460081c99b30a48
cdis001 <shopingid@naver.com>
2021-09-02 13:52:59 +0900

    fix. feed ui  - 대략적인 feed ui 구현

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect, useLayoutEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { faBookmark, faComment, faHeart, faPaperPlane, faSmile } from "@fortawesome/free-regular-svg-icons";
+// import {  } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // import { fetchImages } from "../../redux/actions";
 // import Header from "../Header";
 import "./home.css";
+import "./feed.css";
 import "../../resources/button.css";
 
 const useWindowSize = () => {
@@ -59,9 +63,92 @@ const Home = () => {
       {/* <Header /> */}
       <div className={"home-content-container "}>
         <div className={"home-content-left "}>
-          {/* {feeds.map((feed) => (
-            <FeedPhoto key={feed.id} detailed={false} feed={feed} />
-          ))} */}
+          <article className={"shorted-feed-article"}>
+            <header className={"shorted-feed-header"}>
+              <div className={"feed-header-photo"} />
+              <div className={"shorted-feed-header-content"}>
+                <Link to={`/`} className={"feed-username"}>
+                  accountName01
+                </Link>
+                <p>Location1</p>
+              </div>
+              <button
+                className={"shorted-feed-header-btn"}
+                onClick={(e) => console.log("button")}
+              >
+                ...
+              </button>
+            </header>
+
+            <img
+              className={"shorted-feed-photo"}
+              // src={imgSrc}
+              src={`http://www.astronomer.rocks/news/photo/201802/82361_623_1441.jpeg`}
+              sizes={"614px"}
+              alt={"feedImg"}
+            />
+            <div className={"shorted-feed-activate"}>
+              <div className="feed-actions">
+                <button onClick={() => console.log("123")}>
+                  <FontAwesomeIcon icon={faHeart} className={"feed-action-icons "}/>
+                </button>
+                <button>
+                  <FontAwesomeIcon icon={faComment} className={"feed-action-icons "}/>
+                </button>
+                <button>
+                  <FontAwesomeIcon icon={faPaperPlane} className={"feed-action-icons "}/>
+                </button>
+                <button onClick={() => console.log("123")}>
+                  <FontAwesomeIcon icon={faBookmark} className={"feed-action-icons "}/>
+                </button>
+              </div>
+
+              <div className={"feed-comment"}>
+                <h3 className={"margin-b-8"}>좋아요 3000개</h3>
+                <div className={"shorted-feed-comment-user " + "margin-b-4 "}>
+                  <h3 className={""}>accountName01&nbsp;</h3>
+                  <p>
+                    123123123123123
+                    {/* <button
+                      className={"shorted-feed-comment-btn"}
+                      onClick={() => setIsContentShown(!isContentShown)}
+                      onClick={() => console.log("click")}
+                    >
+                      더 보기
+                    </button> */}
+                  </p>
+                </div>
+                <Link
+                  className={"shorted-feed-comment-btn " + "margin-b-4 "}
+                  to={{ pathname: `/` }}
+                >
+                  댓글 5개 모두 보기
+                </Link>
+                <div className={"shorted-feed-comment-component"}>
+                  <h3>username2</h3>
+                  <p>&nbsp;content</p>
+                <button onClick={() => console.log("123")}>
+                  <FontAwesomeIcon icon={faHeart} className={"feed-comment-icon "}/>
+                  </button>
+                </div>
+                <p className={"datetime"}>4일 전</p>
+              </div>
+              <div className={"feed-comment-box"}>
+                  <FontAwesomeIcon icon={faSmile} className={"feed-action-icons " + "padding-r-16 "}/>
+                <textarea
+                  placeholder={"댓글 달기..."}
+                  // value={commentText}
+                  // onChange={(e) => setCommentText(e.target.value)}
+                />
+                <button
+                  className={"white-blue-btn"}
+                  // disabled={commentText.length > 0 ? false : true}
+                >
+                  게시
+                </button>
+              </div>
+            </div>
+          </article>
         </div>
         <div
           className={"home-content-right "}
@@ -85,7 +172,12 @@ const Home = () => {
             </div>
             <div className={"user-recommend-div "}>
               <div className={"user-recommend-content "}>
-                <div className={"recommend-user-photo "} src={"https://i.guim.co.uk/img/media/976161556e63867b492868c15e86ea71b4165c52/0_165_5315_3189/master/5315.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=b4e203c07941288dab171a18905ad374"} />
+                <img
+                  className={"recommend-user-photo "}
+                  src={
+                    "https://i.guim.co.uk/img/media/976161556e63867b492868c15e86ea71b4165c52/0_165_5315_3189/master/5315.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=b4e203c07941288dab171a18905ad374"
+                  }
+                />
                 <div className={"recommend-user-profile "}>
                   <Link className={"recommend-user-name "} to={"/"}>
                     userName01
