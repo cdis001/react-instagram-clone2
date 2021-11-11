988d41539748f592e656ef9caf015c0d11f2a277
cdis001 <shopingid@naver.com>
2021-11-11 22:10:04 +0900

    UPDATE. feed thumbnail component  - css 수정  - row별로 component 나누기  - component 로 나누기 편하게 css 수정

--- src/components/FeedThumbnail/index.js
+++ src/components/FeedThumbnail/index.js
@@ -1,86 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./feedThumbnail.css";
 
+const RowFeedBox = ({ type = "md", isFloatRight }) => {
+  const classType = type === "lg" ? "-lg" : "-md";
+  const imgSize = type === "lg" ? "height-614" : "height-293";
+  const float = isFloatRight ? "float-r" : "float-l";
+
+  return (
+    <div className={[`feed-thumbnail-box${classType}`]}>
+      <div className={`feed-thumbnail-content${classType} ${float}`}>
+        <img
+          className={`feed-thumbnail-img ${imgSize}`}
+          src={
+            "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg"
+          }
+          srcSet={
+            "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg 1080w"
+          }
+        />
+      </div>
+      <div className={"feed-thumbnail-content-md"}>
+        <img
+          className={"feed-thumbnail-img height-293"}
+          src={
+            "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg"
+          }
+          sizes={"292.984375px"}
+          srcSet={
+            "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg 1080w"
+          }
+        />
+      </div>
+      <div className={"feed-thumbnail-content-md"}>
+        <img
+          className={"feed-thumbnail-img height-293"}
+          src={
+            "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg"
+          }
+          sizes={"292.984375px"}
+          srcSet={
+            "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg 1080w"
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
 const FeedThumbnail = () => {
+  const [isFloatRight, setIsFloatRight] = useState(true);
   return (
     <div className={"feed-thumbnail-container"}>
-      <div className={"feed-thumbnail-box-1"}>
-        <div className={"feed-thumbnail-content-1"}>
-          <img
-            className={"feed-thumbnail-img-big"}
-            src={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg"
-            }
-            sizes={"292.984375px"}
-            srcSet={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg 1080w"
-            }
-          />
-        </div>
-        <div className={"feed-thumbnail-content-2"}>
-          <img
-            className={"feed-thumbnail-img"}
-            src={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg"
-            }
-            sizes={"292.984375px"}
-            srcSet={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg 1080w"
-            }
-          />
-        </div>
-        <div className={"feed-thumbnail-content-2"}>
-          <img
-            className={"feed-thumbnail-img"}
-            src={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg"
-            }
-            sizes={"292.984375px"}
-            srcSet={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg 1080w"
-            }
-          />
-        </div>
-      </div>
-      <div className={"feed-thumbnail-box-2"}>
-        <div className={"feed-thumbnail-content-2"}>
-          <img
-            className={"feed-thumbnail-img"}
-            src={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg"
-            }
-            sizes={"292.984375px"}
-            srcSet={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg 1080w"
-            }
-          />
-        </div>
-        <div className={"feed-thumbnail-content-2"}>
-          <img
-            className={"feed-thumbnail-img"}
-            src={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg"
-            }
-            sizes={"292.984375px"}
-            srcSet={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg 1080w"
-            }
-          />
-        </div>
-        <div className={"feed-thumbnail-content-2"}>
-          <img
-            className={"feed-thumbnail-img"}
-            src={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg"
-            }
-            sizes={"292.984375px"}
-            srcSet={
-              "http://www.jejusori.net/news/photo/202104/328170_336901_1756.jpg 1080w"
-            }
-          />
-        </div>
-      </div>
+      <RowFeedBox type={"lg"} isFloatRight={isFloatRight} />
+      <RowFeedBox type={"md"} />
+      <RowFeedBox type={"md"} />
+      <RowFeedBox type={"md"} />
+      <RowFeedBox type={"md"} />
     </div>
   );
 };
